6a3600790d754b4aca148514e6ba77d5fe3db5ff
Chantz-J <johnson.chantz@gmail.com>
2021-02-23 21:23:09 -0500

    Rendering form submissions to screen

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
 
 import TeamForm from './components/Form'
+import TeamMember from './components/TeamMember'
 
 export default function App(){
 
+  //Empy State for Team Member that will be rendered
+  const Teammembers = []
+
+  const idNumber = Math.random()
   //Shape of default input state
   const formValues = {
     name: '',
     email: '',
-    role: ''
+    role: '',
+    id: idNumber
   }
 
   //State slices: "Team" and "Form" that adds to it.
-  const [team, setTeam] = useState([])
+  const [team, setTeam] = useState(Teammembers)
   const [form, setForm] = useState(formValues)
 
 
@@ -33,6 +39,8 @@ export default function App(){
      if (!newMember.name || !newMember.email || !newMember.role) {
       return
     }
+    setTeam(team.concat(newMember))
+    setForm(formValues)
    }
 
 
@@ -40,6 +48,16 @@ export default function App(){
     <div className="App">
       <h1>Build your team!</h1>
       <TeamForm values={form} update={updateForm} submit={submitForm} />
+      <section className="team-members">
+        <h1>View the team down below: 👇</h1>
+        {
+          team.map(person => {
+            return (
+              <TeamMember key={form.id} info={person}/>
+            )
+          })
+        }
+      </section>
     </div>
   )
 }
--- team-builder/src/components/TeamMember.js
+++ team-builder/src/components/TeamMember.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-export default function TeamMember(){
-
+export default function TeamMember(props){
+    
+    const {info} = props
+    console.log(info.id)
     
     return (
         <section className="team-member">
-
+            <div className="icon">👨‍💻</div>
+            <h1>{info.name}</h1>
+            <h2>{info.email}</h2>
+            <h2>{info.role}</h2>
         </section>
     )
 }
